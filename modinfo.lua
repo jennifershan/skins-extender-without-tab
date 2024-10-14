4ca901a1fddaba4a9b3fb40d190d5fbfca6d2a25
jennifershan <js8@wellesley.edu>
2024-10-14 01:16:02 -0400

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,15 @@
+name = "Skins extender without tab"
+author = "Cunning fox, Myxal"
+version = "1.3.1"
+description = "Automatically opens your skins without science machine!\nVersion: "..version
+forumthread = ""
+
+client_only_mod = true
+dst_compatible = true
+
+icon_atlas = "preview.xml"
+icon = "preview.tex"
+
+api_version = 10
+
+priority = -0.681239
